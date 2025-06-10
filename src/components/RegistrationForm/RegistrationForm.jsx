af3225187fda5fe1ea0101fe396c5dc4b91fd90e
Anastasiia Posliednicheko <anastasia29po@gmail.com>
2025-06-10 22:35:14 +0200

    hw

--- src/components/RegistrationForm/RegistrationForm.jsx
+++ src/components/RegistrationForm/RegistrationForm.jsx
@@ -3,7 +3,7 @@ import { useNavigate } from "react-router-dom";
 import { Formik, Form, Field, ErrorMessage  } from "formik";
 import * as Yup from "yup";
 import {register} from "../../redux/auth/operations";
-import css from "../RegistrationForm/RegistrationForm.module.css";
+
 
 
 const validationSchema = Yup.object({
